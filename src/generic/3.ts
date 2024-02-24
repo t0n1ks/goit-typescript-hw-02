ef4adcc6361ab3ec778f156792f2c0c5432adb9f
Nikita Titov <nikitatitov255@gmail.com>
2024-02-24 17:46:25 +0200

    hw2_fix 0.1

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,9 +3,10 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
+
 
 // Приклад використання виправленної функції merge
 // const obj1 = { name: 'Obj1', age: 30 };
